17d629919800c7b479d6fc3304534d80ef53ec44
jimmy4701 <jeremy.simonklein@gmail.com>
2025-02-05 14:47:49 +0100

    Finished development for add cookie on image click

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 const App = () => {
+
+  const [cookies, setCookies] = useState(0)
+
 
   return (
     <div className="w-screen h-screen flex">
-      <div className="flex-1 flex justify-center items-center">
-        <img className="h-40" src="https://cdn-icons-png.flaticon.com/512/216/216341.png" alt="cookie" />
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <p>Total cookies : {cookies}</p>
+        <img 
+          onClick={() => { setCookies(cookies + 1)}} 
+          className="h-40" 
+          src="https://cdn-icons-png.flaticon.com/512/216/216341.png" 
+          alt="cookie" />
       </div>
       <div className="flex-1 bg-red-100">items</div>
       <div className="flex-1 bg-blue-100">shop</div>
