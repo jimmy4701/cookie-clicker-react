0127e3f5b56983c3392151c89fed2866d7af2ff6
jimmy4701 <jeremy.simonklein@gmail.com>
2025-02-05 15:10:19 +0100

    Added display of basic shop items

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react"
 
+const shop_items = [
+  {name: "Grand mère", value: 100, cps: 1},
+  {name: "Ferme à cookie", value: 1000, cps: 10},
+  {name: "Usine à cookie", value: 10000, cps: 100},
+]
+
 const App = () => {
 
   const [cookies, setCookies] = useState(0)
@@ -16,7 +22,11 @@ const App = () => {
           alt="cookie" />
       </div>
       <div className="flex-1 bg-red-100">items</div>
-      <div className="flex-1 bg-blue-100">shop</div>
+      <div className="flex-1 bg-blue-100">
+        {shop_items.map(item => {
+          return <p>{item.name}</p>
+        })}
+      </div>
     </div>
   )
 }
